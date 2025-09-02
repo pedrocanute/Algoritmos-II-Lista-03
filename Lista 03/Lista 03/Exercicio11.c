7c16344d53b6a13caa69f36ac1183f81be9ff454
pedrocanute <pedro.canute2@gmail.com>
2025-09-02 17:35:16 -0300

    Adicionando exercicio 11

---
+++ Lista 03/Lista 03/Exercicio11.c
@@ -0,0 +1,32 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//#include <stdlib.h>
+//
+////Escreva a função recursiva que recebe um número inteiro e imprime seu valor binário na tela.
+//
+//void calcular_binario(int n) {
+//
+//	if (n < 2) {
+//		printf("%d", n);
+//		return;
+//	}
+//	else {
+//		calcular_binario(n / 2);
+//		printf("%d", n % 2);
+//	}
+//	
+//
+//}
+//
+//int main() {
+//
+//	int n;
+//
+//	printf("Digite um numero e veja seu equivalente em binario: ");
+//	scanf("%d", &n);
+//
+//	printf("%d em binario eh: ", n);
+//	calcular_binario(n);
+//
+//	return 0;
+//}
